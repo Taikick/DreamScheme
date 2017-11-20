12956fb9d5e08553829becbb8fc01095c10861e0
Taiki__ck <tensainanoda@gmail.com>
2017-11-21 02:16:59 +0800

    アナライズのカスタム進行中

---
+++ DreamScheme/AnalizeTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AnalizeTableViewCell.swift
+//  DreamScheme
+//
+//  Created by 加藤　大起 on 2017/11/21.
+//  Copyright © 2017年 Taiki Kato. All rights reserved.
+//
+
+import UIKit
+
+class AnalizeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var AnaizeListLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- DreamScheme/AnalizeViewController.swift
+++ DreamScheme/AnalizeViewController.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
-class AnalizeViewController: UIViewController {
+class AnalizeViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    var userTasks:[String] = ["英語でナンパ","プログラミング"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    /// セルの個数を指定するデリゲートメソッド（必須）
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userTasks.count
+    }
+    
+    /// セルに値を設定するデータソースメソッド（必須）
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tasksCell = tableView.dequeueReusableCell(withIdentifier: "AnalizeCell") as! AnalizeTableViewCell
+        tasksCell.AnaizeListLabel.text = userTasks[indexPath.row]
+        
+        return tasksCell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
