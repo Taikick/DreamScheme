ee67d12e444e50b276376c1f3eb756db6320f2bd
Taiki__ck <tensainanoda@gmail.com>
2017-11-21 22:01:35 +0800

    グラフできた

--- DreamScheme/AnalizeViewController.swift
+++ DreamScheme/AnalizeViewController.swift
@@ -14,13 +14,13 @@ class AnalizeViewController: UIViewController,UITableViewDelegate,UITableViewDat
     var userTasks:[String] = ["英語でナンパ","プログラミング"]
     
     @IBOutlet weak var radarChartView: RadarChartView!
-    var dataEntries: [ChartDataEntry] = []
+
 
     var subjects = [
-        "国語", "英語", "数学", "理科", "社会"
+        "ナンパ", "金", "女", "XX", "プログラミング"
     ]
     //点数
-    let array = [100.0, 100.0, 100.0, 100.0, 100.0]
+    let array = [10.0, 50.0, 80.0, 100.0, 100.0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,33 +33,36 @@ class AnalizeViewController: UIViewController,UITableViewDelegate,UITableViewDat
         radarChartView.noDataText = "You need to provide data for the chart."
         var dataEntries: [ChartDataEntry] = []
         for i in 0..<dataPoints.count {
-            let dataEntry = ChartDataEntry(x: values[i], y: Double(i))
+            let dataEntry = ChartDataEntry(x:Double(i) , y: values[i])
             dataEntries.append(dataEntry)
+            print(dataEntries)
         }
         let chartDataSet = RadarChartDataSet(values: dataEntries, label: "Units Sold")
         let chartData = RadarChartData(dataSet: chartDataSet)
-        radarChartView.data = chartData
+        
         //radarChartのオプション
         radarChartView.sizeToFit()
         radarChartView.descriptionText = ""
         
         //ここから軸の設定。表示範囲は0から100までとし、20刻みでグリッド線を入れる
-        radarChartView.yAxis.labelCount = 6
-        radarChartView.yAxis.axisMinValue = 0.0
+        radarChartView.yAxis.labelCount = 2
+        radarChartView.yAxis.axisMinValue = 0
         radarChartView.yAxis.axisMaxValue = 100.0
         //ここまで軸の設定
-        
+        radarChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values:subjects)
+        radarChartView.xAxis.granularity = 1
         radarChartView.rotationEnabled = false
         chartDataSet.drawFilledEnabled = true
         //値は整数で表示
         let numberFormatter = NumberFormatter()
         numberFormatter.generatesDecimalNumbers = false
-        chartDataSet.valueFormatter = NumberFormatter() as? IValueFormatter
+        chartDataSet.valueFormatter = numberFormatter as? IValueFormatter
         radarChartView.yAxis.valueFormatter = numberFormatter as? IAxisValueFormatter
         //その他のオプション!
         radarChartView.legend.enabled = false
         radarChartView.yAxis.gridAntialiasEnabled = true
-        radarChartView.animate(yAxisDuration: 2.0)
+        radarChartView.animate(yAxisDuration: 1.0)
+        radarChartView.data = chartData
     }
 
     
