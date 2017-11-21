db3e75d58e4eb18b101aeffed45c525581894cf8
Taiki__ck <tensainanoda@gmail.com>
2017-11-21 15:44:06 +0800

    怖いから一旦

--- DreamScheme/editViewController.swift
+++ DreamScheme/editViewController.swift
@@ -7,14 +7,81 @@
 //
 
 import UIKit
+import Charts
 
-class editViewController: UIViewController {
+
+class editViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     var passedIndex = -1
+    
+    @IBOutlet weak var DtitleTableView: UITableView!
+    var entry = [
+        
+        [BarChartDataEntry(x: 1.0, y: 3.0)],
+        [BarChartDataEntry(x: 1.0, y: 3.0)],
+        [BarChartDataEntry(x: 1.0, y: 100.0)]
+        
+    ]
+    
+    
+    var DTitle:[String] = [
+        "英語を勉強してナンパできるようになる"
+    ]
+    
+    var DTitleTime:[String] = [
+        "2017.12.24-2018.12.24"
+    ]
+
+    var ProTitle:[String] = [
+        "英語を勉強してナンパできるようになる"
+    ]
+    
+    var ProTime:[String] = [
+        "2017.12.24-2018.12.24"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+
+    //行数の決定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DTitle.count;
+    }
+    
+    //セルに表示する文字列の決定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = DtitleTableView.dequeueReusableCell(withIdentifier: "DTitleCell", for: indexPath) as! DTitleTableViewCell
+        if tableView.tag == 0 {
+            cell.DTitleLabel.text = DTitle[indexPath.row]
+            cell.DTitleDate.text = DTitleTime[indexPath.row]
+            var rect = cell.DTitleChart.bounds
+            rect.origin.y += 4
+            rect.size.height -= 4
+            let chartView = HorizontalBarChartView(frame: rect)
+            let set = BarChartDataSet(values: entry[1], label: "タスク時間")
+            chartView.data = BarChartData(dataSet: set)
+            chartView.drawBordersEnabled = false
+            chartView.minOffset = CGFloat(10.0)
+            chartView.sizeToFit()
+            //x軸の設
+            chartView.xAxis.drawGridLinesEnabled = false
+            chartView.xAxis.forceLabelsEnabled = false
+            chartView.xAxis.drawLabelsEnabled = false
+            //        chartView.xAxis.labelCount = 100
+            //        chartView.xAxis.axisMinimum = 1
+            //y軸の設定
+            chartView.leftAxis.axisMinimum = 0
+            chartView.rightAxis.axisMaximum = 100
+            set.formLineWidth = 3
+            set.formSize = 10
+            cell.DTitleChart.addSubview(chartView)
+            
+        } else {
+            
+        }
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
