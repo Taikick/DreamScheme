e5e1ccc34bd09c19db4a0733dfda11087ce25b40
Taiki__ck <tensainanoda@gmail.com>
2017-11-27 23:28:08 +0800

    画像処理試み

--- DreamScheme/CreateViewController.swift
+++ DreamScheme/CreateViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 import CoreData
+import Photos
+import MobileCoreServices
 
-class CreateViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewDataSource,UITextFieldDelegate{
+class CreateViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewDataSource,UITextFieldDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
     @IBOutlet weak var taskImage: UIImageView!
     
     @IBOutlet weak var createTextFiled: UITextField!
@@ -305,7 +307,83 @@ class CreateViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewD
     }
     
     
+    @IBAction func tapImage(_ sender: UITapGestureRecognizer) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {    //追記
+            print("行けたお")
+            //写真ライブラリ(カメラロール)表示用のViewControllerを宣言
+            let controller = UIImagePickerController()
+            
+            controller.delegate = self
+            //新しく宣言したViewControllerでカメラとカメラロールのどちらを表示するかを指定
+            controller.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            //トリミング
+            controller.allowsEditing = true
+            //新たに追加したカメラロール表示ViewControllerをpresentViewControllerにする
+            self.present(controller, animated: true, completion: nil)
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+                
+                let picker = UIImagePickerController()
+                picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+                picker.mediaTypes = [kUTTypeMovie as String]
+                picker.allowsEditing = false
+                picker.delegate = self
+                picker.videoQuality = UIImagePickerControllerQualityType.typeHigh
+                
+                self.present(picker, animated: true, completion: nil)
+            }
+            
+            //UserDefaultから取り出す
+            // ユーザーデフォルトを用意する
+            let myDefault = UserDefaults.standard
+            
+            // データを取り出す
+            let strURL = myDefault.string(forKey: "selectedPhotoURL")
+            
+            if strURL != nil{
+                
+                let url = URL(string: strURL as String!)
+                let fetchResult: PHFetchResult = PHAsset.fetchAssets(withALAssetURLs: [url!], options: nil)
+                let asset: PHAsset = (fetchResult.firstObject! as PHAsset)
+                let manager: PHImageManager = PHImageManager()
+                manager.requestImage(for: asset,targetSize: CGSize(width: 5, height: 500),contentMode: .aspectFill,options: nil) { (image, info) -> Void in
+                    self.taskImage.image = image
+                }
+                
+            }
+            
+        }
+
+        
+    }
     
+    //カメラロールで写真を選んだ後
+    func imagePickerController(_ imagePicker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        
+        
+        let assetURL:AnyObject = info[UIImagePickerControllerReferenceURL]! as AnyObject
+        
+        let strURL:String = assetURL.description
+        
+        print(strURL)
+        
+        
+        // ユーザーデフォルトを用意する
+        let myDefault = UserDefaults.standard
+        
+        // データを書き込んで
+        myDefault.set(strURL, forKey: "selectedPhotoURL")
+        
+        // 即反映させる
+        myDefault.synchronize()
+        
+        
+        
+        //閉じる処理
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
     
 
     //キーボードを出た時に下がる処理
